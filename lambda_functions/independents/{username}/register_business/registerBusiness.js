aa837a6732a0c5f1d25d3737317193a6626b7b2b
Jack Ralph <jackralph@Jacks-MBP.local>
2020-05-28 11:27:47 +0100

    error now available for a postCode that can not be found in the database

--- lambda_functions/independents/{username}/register_business/registerBusiness.js
+++ lambda_functions/independents/{username}/register_business/registerBusiness.js
@@ -7,15 +7,17 @@ exports.handler = async (event, context) => {
 
   let responseBody = "";
   let statusCode = 0;
+  let coordinates;
+  let coordinatesError = false;
 
   const {
     businessName,
+    businessEmail,
     postCode,
     address,
     businessType,
     logoUrl,
     about,
-    tables,
     cuisine,
     vegan,
     vegetarian,
@@ -30,91 +32,115 @@ exports.handler = async (event, context) => {
     instagram,
   } = JSON.parse(event.body);
 
+  const coordsPostCode = JSON.parse(event.body);
+
   const { username } = event.pathParameters;
 
-  const coordinates = await axios.get(
-    `https://api.postcodes.io/postcodes/${postCode}`
-  );
-
-  const params = {
-    TableName: "Independents",
-    Key: { username },
-    UpdateExpression:
-      "SET #bn = :bn, #pc = :pc, #ad = :ad, #bt = :bt, #cu = :cu, #ve = :ve, #vg = :vg, #ha = :ha, #gf = :gf, #lu = :lu, #ab = :ab, #ta = :ta, #la = :la, #lo = :lo, #ba = :ba, #me = :me, #up = :up, #pn = :pn, #fb = :fb, #tw = :tw, #ig = :ig, #hb = :hb, #vf = :vf, #co = :co, #vo = :vo",
-    ExpressionAttributeNames: {
-      "#bn": "businessName",
-      "#pc": "postCode",
-      "#ad": "address",
-      "#bt": "businessType",
-      "#cu": "cuisine",
-      "#ve": "vegan",
-      "#vg": "vegetarian",
-      "#ha": "halal",
-      "#gf": "glutenFree",
-      "#lu": "logoUrl",
-      "#ab": "about",
-      "#ta": "tables",
-      "#la": "latitude",
-      "#lo": "longitude",
-      "#ba": "banner",
-      "#me": "menu",
-      "#up": "updates",
-      "#pn": "phoneNumber",
-      "#fb": "facebook",
-      "#tw": "twitter",
-      "#ig": "instagram",
-      "#hb": "hasBusiness",
-      "#co": "comments",
-      "#vf": "verified",
-      "#vo": "votes",
-    },
-    ExpressionAttributeValues: {
-      ":bn": businessName || null,
-      ":pc": postCode || null,
-      ":ad": address || null,
-      ":bt": businessType || null,
-      ":cu": cuisine || null,
-      ":ve": vegan || null,
-      ":vg": vegetarian || null,
-      ":ha": halal || null,
-      ":gf": glutenFree || null,
-      ":lu": logoUrl || null,
-      ":ab": about || null,
-      ":ta": 0,
-      ":la": coordinates.data.result.latitude || null,
-      ":lo": coordinates.data.result.longitude || null,
-      ":ba": banner || null,
-      ":me": menu || null,
-      ":up": updates || null,
-      ":pn": phoneNumber || null,
-      ":fb": facebook || null,
-      ":tw": twitter || null,
-      ":ig": instagram || null,
-      ":hb": "yes",
-      ":co": [],
-      ":vf": "no",
-      ":vo": 0,
-    },
-    ReturnValues: "UPDATED_NEW",
-  };
-
   try {
-    const data = await documentClient.update(params).promise();
-    responseBody = data;
-    statusCode = 200;
+    coordinates = await axios.get(
+      `https://api.postcodes.io/postcodes/${coordsPostCode.postCode}`
+    );
   } catch (error) {
-    responseBody = `Unable to register business: ${error}`;
+    console.log(error);
+    coordinatesError = true;
+    responseBody = `${postCode} can not be found: request failed with a status 400`;
     statusCode = 400;
   }
 
-  const response = {
-    statusCode: statusCode,
-    headers: {
-      "Content-Type": "applications/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-    body: responseBody,
-  };
+  if (coordinatesError) {
+    const response = {
+      statusCode: statusCode,
+      headers: {
+        "Content-Type": "applications/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(responseBody),
+    };
 
-  return response;
+    return response;
+  } else {
+    const params = {
+      TableName: "Independents",
+      Key: { username },
+      UpdateExpression:
+        "SET #bn = :bn, #be = :be, #pc = :pc, #ad = :ad, #bt = :bt, #cu = :cu, #ve = :ve, #vg = :vg, #ha = :ha, #gf = :gf, #lu = :lu, #ab = :ab, #ta = :ta, #la = :la, #lo = :lo, #ba = :ba, #me = :me, #up = :up, #pn = :pn, #fb = :fb, #tw = :tw, #ig = :ig, #hb = :hb, #vf = :vf, #co = :co, #vo = :vo",
+      ExpressionAttributeNames: {
+        "#bn": "businessName",
+        "#be": "businessEmail",
+        "#pc": "postCode",
+        "#ad": "address",
+        "#bt": "businessType",
+        "#cu": "cuisine",
+        "#ve": "vegan",
+        "#vg": "vegetarian",
+        "#ha": "halal",
+        "#gf": "glutenFree",
+        "#lu": "logoUrl",
+        "#ab": "about",
+        "#ta": "tables",
+        "#la": "latitude",
+        "#lo": "longitude",
+        "#ba": "banner",
+        "#me": "menu",
+        "#up": "updates",
+        "#pn": "phoneNumber",
+        "#fb": "facebook",
+        "#tw": "twitter",
+        "#ig": "instagram",
+        "#hb": "hasBusiness",
+        "#co": "comments",
+        "#vf": "verified",
+        "#vo": "votes",
+      },
+      ExpressionAttributeValues: {
+        ":bn": businessName || null,
+        ":be": businessEmail || null,
+        ":pc": postCode || null,
+        ":ad": address || null,
+        ":bt": businessType || null,
+        ":cu": cuisine || null,
+        ":ve": vegan || null,
+        ":vg": vegetarian || null,
+        ":ha": halal || null,
+        ":gf": glutenFree || null,
+        ":lu": logoUrl || null,
+        ":ab": about || null,
+        ":ta": 0,
+        ":la": coordinates.data.result.latitude || null,
+        ":lo": coordinates.data.result.longitude || null,
+        ":ba": banner || null,
+        ":me": menu || null,
+        ":up": updates || null,
+        ":pn": phoneNumber || null,
+        ":fb": facebook || null,
+        ":tw": twitter || null,
+        ":ig": instagram || null,
+        ":hb": "yes",
+        ":co": [],
+        ":vf": "no",
+        ":vo": 0,
+      },
+      ReturnValues: "UPDATED_NEW",
+    };
+
+    try {
+      const data = await documentClient.update(params).promise();
+      responseBody = data;
+      statusCode = 200;
+    } catch (error) {
+      responseBody = `Unable to register business: ${error}`;
+      statusCode = 400;
+    }
+
+    const response = {
+      statusCode: statusCode,
+      headers: {
+        "Content-Type": "applications/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(responseBody),
+    };
+
+    return response;
+  }
 };
